cc220e04c1d76642d3b84bbfb769525af5120b56
Studenti <enesmulolli59@gmail.com>
2024-11-16 12:17:04 +0100

    ...

--- lesson4/main.py
+++ lesson4/main.py
@@ -1,13 +1,26 @@
-#create a list
+# Create a list
+names = ["Blerta", "Erosi", "Driloni", "Brikena", "Ylli"]
 
-names = ["Blerta", "Erosi", "Driloni", "Brikena","Ylli"]
-
+# Loop through the list and print each name
 for name in names:
     print(name)
 
-
+# Sentence and print only alphabetic characters
 sentence = "Hello World"
-
 for character in sentence:
-    if character.isalpha(): #cheks if the character is a letter
+    if character.isalpha():  # Checks if the character is a letter
         print(character)
+
+# Range function (example from 0 to 4)
+for number in range(5):  # This will iterate from 0 to 4
+    print(number)
+
+# Finding the maximum value in the list of numbers
+numbers = [12, 45, 6, 72, 8, 94, 57]
+maximum = numbers[0]
+
+for num in numbers:  # Loop through each element in the list "numbers"
+    if num > maximum:  # Check if the current element "num" is greater than the current maximum value
+        maximum = num  # If so, update the maximum value to be the current element in num
+
+print("The maximum value in the list is", maximum)
